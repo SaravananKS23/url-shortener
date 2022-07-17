444f147733d1691828f3666cc03ecc63490264c8
SaravananKS23 <91030871+SaravananKS23@users.noreply.github.com>
2022-07-17 23:35:41 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('mongodb://localhost/urlShortener', {
+mongoose.connect('mongodb+srv://Saran23:Saranm23@cluster0.89qlcus.mongodb.net/urlshortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
@@ -31,4 +31,4 @@ app.get('/:shortUrl', async (req, res) => {
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(5000);
